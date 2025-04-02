361c84eb9cee6246c3cb5fcdba666814554a432c
Richardlws <liu.bing66066@hotmail.com>
2025-04-02 17:57:23 +0800

    create output

---
+++ output.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OK!");
+        }
+
+    }
+
+}
