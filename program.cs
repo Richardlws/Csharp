8b5288d1d1502a536163817ff265a9b9d2c3a998
Richardlws <liu.bing66066@hotmail.com>
2025-04-02 17:54:22 +0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I like pizza!");
+            Console.WriteLine("It's really good!");
+            Console.WriteLine("It's really awesome!");
+            Console.Beep();
+        }
+    }
+}
