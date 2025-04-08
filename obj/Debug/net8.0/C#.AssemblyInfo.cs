35916d3db93289e499493593b3ef927b836c832e
Richardlws <liu.bing66066@hotmail.com>
2025-04-08 12:56:20 +0800

    normalmodify

--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78c1e5217ab936de41fa8d5330385daa0f8de30f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50afc92ff8259abb2a4f67202ee6a1e87976cbb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
