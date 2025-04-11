c0e186501639d8fe49999cb92b0e5af6bb2088d4
Richardlws <liu.bing66066@hotmail.com>
2025-04-11 12:52:31 +0800

    if statement

---
+++ ifstatement.cs
@@ -0,0 +1,17 @@
+using System;
+namespace ifstatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if statement = a basic form of decision making
+
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            
+
+        }
+    }
+}
