9cb7a5bb2bef54400aef309f7f2bee28a6a4fbef
Richardlws <liu.bing66066@hotmail.com>
2025-04-15 21:35:15 +0800

    Create numberguessgame.cs

---
+++ numberguessgame.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace numberguessgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+        }
+    }
+}
