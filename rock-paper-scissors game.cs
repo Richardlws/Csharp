1a7adf6ab40f23bc08e625fac565acdf6c2e6c24
Richardlws <liu.bing66066@hotmail.com>
2025-04-15 21:55:51 +0800

    Create rock-paper-scissors game.cs

---
+++ rock-paper-scissors game.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace rockpapaerscissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+
+            
+        }
+    }
+}
