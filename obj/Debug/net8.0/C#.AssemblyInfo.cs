55bc5224c7373dc08f4003a14e84a68b37ae273a
Richardlws <liu.bing66066@hotmail.com>
2025-04-16 13:05:27 +0800

    calculator

--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf322b8d2fdfe339d59ec36eec0bdca0015de216")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387ba4145355e826c2e9dc107c43880c0b30ef86")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
