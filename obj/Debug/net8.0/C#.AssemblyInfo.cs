8c917e8cfc20594dc2cb4a2f14bf8f4ae16daad5
Richardlws <liu.bing66066@hotmail.com>
2025-04-18 12:42:57 +0800

    methodoverloading

--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a37f9faf39d8d685dace0bd207b0112b5a8cf76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6def4ba1da21527879a12fb423f57bce4553540")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
