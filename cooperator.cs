118670d0637b181be504a5c09954e756dbb60e25
Richardlws <liu.bing66066@hotmail.com>
2025-04-18 13:08:41 +0800

    conditional operator

---
+++ cooperator.cs
@@ -0,0 +1,17 @@
+using System;
+namespace  conditionaloperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //conditional operator = used in conditional assignment if a condition is true/false
+            //(condition)? x : y
+
+            
+
+
+
+        }
+    }
+}
