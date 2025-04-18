d8001a43a41c4f8854cc458d960e1b6b53821b76
Richardlws <liu.bing66066@hotmail.com>
2025-04-18 12:56:41 +0800

    normal modify

--- exceptionhandling.cs
+++ exceptionhandling.cs
@@ -1,11 +1,29 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 namespace exceptionhandling
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            //exception = errors that occur during execution
+            //  try = try some code that is considered "dangerous"
+            //  chat = catches and handles exceptions when they occur
+            //  finally = always executes regardless if exception is caught or not
+
+            double x;
+            double y;
+            double result;
+
+            Console.WriteLine("Enter number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = x / y;
+            Console.WriteLine("result: " + result);
+
         }
     }
 }
--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d462d081c660839d156fae3a716bac48c5d6d21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9c3ac31f97c569bdb03a19399d1d13ad427770")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
