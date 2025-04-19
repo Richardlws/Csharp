e13431e93f163c37ff415c74c1ac859f15d18e60
Richardlws <liu.bing66066@hotmail.com>
2025-04-19 13:22:41 +0800

    inheritance

--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a35f77d471fbf4585bc2a5f969e9bfbf79d55f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c39f1e564f327744aac4339d9a790ca154039ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
