4a35f77d471fbf4585bc2a5f969e9bfbf79d55f4
Richardlws <liu.bing66066@hotmail.com>
2025-04-19 12:00:55 +0800

    static

--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8875838c52b0b62f8e8958296537787cbe3f2c6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2137527bd3b866aeca4ff296dc8d3a32d5ee06d")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
