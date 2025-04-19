303414d26ef3e952c3ca4615b8702eb8367affb5
Richardlws <liu.bing66066@hotmail.com>
2025-04-19 16:17:44 +0800

    methodoveringriding

---
+++ methondoverriding.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ObjectsAsArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //method overriding = provides a new version of a method inherited from a parent class
+            //                      inherited method must be :abstract,virtual, or already overriden
+            //                      used with tostring(),polymorphism
+
+            
+        }
+}
